6bc369d2eb2c118d5aba8abc8686a4ada36be2d7
Keith Wang <kewang2011fall@gmail.com>
2023-10-10 15:54:01 +0000

    bastion stack

--- infra/lib/bastion-stack.ts
+++ infra/lib/bastion-stack.ts
@@ -4,30 +4,29 @@ import { Role, ServicePrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 interface BastionStackProps extends cdk.StackProps {
-  vpc: Vpc;
+    vpc: Vpc;
 }
 
 export class BastionStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: BastionStackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props: BastionStackProps) {
+        super(scope, id, props);
 
-    const vpc = props.vpc;
+        const vpc = props.vpc;
 
-    // Define a role for the instance that allows it to be managed by SSM
-    const ssmRole = new Role(this, 'SSMInstanceRole', {
-      assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
-    });
+        // Define a role for the instance that allows it to be managed by SSM
+        const ssmRole = new Role(this, 'SSMInstanceRole', {
+            assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
+        });
 
-    ssmRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2RoleforSSM'));
-    ssmRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonEC2RoleforSSM'));
+        ssmRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2RoleforSSM'));
 
-    // Create the EC2 instance
-    new Instance(this, 'BastionHost', {
-      vpc: vpc,
-      instanceType: new InstanceType('t2.micro'),
-      machineImage: MachineImage.latestAmazonLinux2(), // Use latest Amazon Linux AMI
-      vpcSubnets: { subnetType: SubnetType.PRIVATE_ISOLATED }, // Place in the private subnet
-      role: ssmRole
-    });
-  }
+        // Create the EC2 instance
+        new Instance(this, 'BastionHost', {
+            vpc: vpc,
+            instanceType: new InstanceType('t2.micro'),
+            machineImage: MachineImage.latestAmazonLinux2(), // Use latest Amazon Linux AMI
+            vpcSubnets: { subnetType: SubnetType.PRIVATE_ISOLATED }, // Place in the private subnet
+            role: ssmRole
+        });
+    }
 }
