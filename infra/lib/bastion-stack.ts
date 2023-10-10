fe918646f62300e2a68655ee87e3d1ca4b7b9819
Keith Wang <kewang2011fall@gmail.com>
2023-10-10 19:52:36 +0000

    updata bastion host

--- infra/lib/bastion-stack.ts
+++ infra/lib/bastion-stack.ts
@@ -1,7 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
-import { Vpc, Instance, InstanceType, MachineImage, SubnetType } from 'aws-cdk-lib/aws-ec2';
-import { Role, ServicePrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
+import { Instance, InstanceType, MachineImage, Vpc, SecurityGroup, Peer, Port, SubnetType } from 'aws-cdk-lib/aws-ec2';
 
 interface BastionStackProps extends cdk.StackProps {
     vpc: Vpc;
@@ -13,14 +12,24 @@ export class BastionStack extends cdk.Stack {
 
         const vpc = props.vpc;
 
+        // Create a security group that allows inbound SSH traffic
+        const bastionSecurityGroup = new SecurityGroup(this, 'BastionSecurityGroup', {
+            vpc: vpc,
+            description: 'Allow inbound SSH access',
+            allowAllOutbound: true   // Default is true, but it's a good practice to specify it.
+        });
+
+        bastionSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22), 'Allow SSH access from anywhere');
+
         // Create the EC2 instance
         new Instance(this, 'BastionHost', {
             vpc: vpc,
             instanceType: new InstanceType('t2.micro'),
             machineImage: MachineImage.latestAmazonLinux2(), // Use latest Amazon Linux AMI
             vpcSubnets: { subnetType: SubnetType.PRIVATE_ISOLATED }, // Place in the private subnet
-            keyName: 'my-keypair'
+            keyName: 'my-keypair',
+            securityGroup: bastionSecurityGroup
         });
-
     }
 }
+
