6dad3d37a9bdd5c089042cb063160f7bc015fd02
Keith Wang <kewang2011fall@gmail.com>
2023-10-17 15:54:22 +0000

    included sftp efs stack

--- infra/lib/sftp-efs-stack.ts
+++ infra/lib/sftp-efs-stack.ts
@@ -55,6 +55,7 @@ export class SftpEfsStack extends cdk.Stack {
             userName: 'mySftpUser',
             role: sftpRole.roleArn,
             homeDirectory: `/${accessPoint.accessPointId}`,
+            sshPublicKeys: ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCWCxQj8spgIpPJchfRj8SORq26hgFng2XmKdTd5NLGUqy6el8nf4p8/rXh4nrkgE+s0fegglrp2+XX6jQXfexLshWhYQxDNQCfTHqddL0WfnUrFjb0oste5D22HmLD1+ODyLkl8guaISDFgKNqWjT8T6V1Cj2tmdK2n73imbv1X6oQfwMMPwliVGIDlk1N1xnQDzDOknP5rN9WAJn+SermtxbteQ4/IJdDIGVdJsdOipk7uIK7ZpeBtr4ockLyVnj5TrcQFiqlhOVSy0YbsNytDWM+NpmuI4hTOp13Y6uXaCDQ6i7gFCgxOla9amgP4WASviGSXBGVeuQnfNOtkillKnKJ101qWkNrrIquO2n0QvjHKvDhG4PzRaBro1tsPPgCbo/npRg3Uq53hqn6ueuML+OSVFK/dX/AQx+ZxrHtG7pCHKaT4CprV7nY9k7wN9mh3De3sIzZWy4rSDS/Uk74MvuYP6QRYyqZnINwctqcxCGSjWZlMHSjAE66pzBXG/qITeZvHcbIkQ+6gsXGdqPbvcQq0JnLZzRCWKOAS6ibqliO+3lqICsk5heQidKrmLyzFY2tA1U8Oclp+Ph0DObxobb1DALtbbXlG1Lp+SGBjZWNvJKzYNspAneGwDbum3G1+VD8V7XicC8qfI/JgYJl6TCLJa1JARti2KI2LRPWlw== your_email@example.com'],  // Add this line
         });
 
         sftpUser.node.addDependency(sftpServer);
