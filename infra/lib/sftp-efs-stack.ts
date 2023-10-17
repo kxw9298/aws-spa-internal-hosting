543549d889e77286b20f6a3722c76f94b7ba7f88
Keith Wang <kewang2011fall@gmail.com>
2023-10-17 15:16:45 +0000

    included sftp efs stack

--- infra/lib/sftp-efs-stack.ts
+++ infra/lib/sftp-efs-stack.ts
@@ -6,63 +6,66 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 interface SftpEfsStackProps extends cdk.StackProps {
-  vpc: ec2.IVpc;
+    vpc: ec2.IVpc;
 }
 
 export class SftpEfsStack extends cdk.Stack {
-  public readonly fileSystem: efs.FileSystem;
+    public readonly fileSystem: efs.FileSystem;
 
-  constructor(scope: Construct, id: string, props: SftpEfsStackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props: SftpEfsStackProps) {
+        super(scope, id, props);
 
-    const vpc = props.vpc;
+        const vpc = props.vpc;
 
-    // Create an EFS FileSystem
-    const fileSystem = new efs.FileSystem(this, 'MyEfsFileSystem', {
-      vpc,
-      lifecyclePolicy: efs.LifecyclePolicy.AFTER_7_DAYS,
-    });
+        // Create an EFS FileSystem
+        const fileSystem = new efs.FileSystem(this, 'MyEfsFileSystem', {
+            vpc,
+            lifecyclePolicy: efs.LifecyclePolicy.AFTER_7_DAYS,
+        });
 
-    // Create an EFS Access Point with a specified path
-    const accessPoint = new efs.AccessPoint(this, 'EfsAccessPoint', {
-      fileSystem: fileSystem,
-      path: '/spa',  // Specify the path for the access point
-      createAcl: {
-        ownerGid: '1000',
-        ownerUid: '1000',
-        permissions: '755',
-      },
-    });
+        // Create an EFS Access Point with a specified path
+        const accessPoint = new efs.AccessPoint(this, 'EfsAccessPoint', {
+            fileSystem: fileSystem,
+            path: '/spa',  // Specify the path for the access point
+            createAcl: {
+                ownerGid: '1000',
+                ownerUid: '1000',
+                permissions: '755',
+            },
+        });
 
-    // SFTP user's role
-    const sftpRole = new iam.Role(this, 'SftpRole', {
-      assumedBy: new iam.ServicePrincipal('transfer.amazonaws.com'),
-    });
+        // SFTP user's role
+        const sftpRole = new iam.Role(this, 'SftpRole', {
+            assumedBy: new iam.ServicePrincipal('transfer.amazonaws.com'),
+        });
 
-    fileSystem.grant(sftpRole, 'elasticfilesystem:ClientWrite');
+        fileSystem.grant(sftpRole, 'elasticfilesystem:ClientWrite');
 
-    // Create AWS Transfer for SFTP Server 
-    const sftpServer = new transfer.CfnServer(this, 'SftpServer', {
-      endpointType: 'PUBLIC',
-      identityProviderType: 'SERVICE_MANAGED',
-      protocols: ['SFTP'],
-      loggingRole: sftpRole.roleArn,
-    });
+        // Create AWS Transfer for SFTP Server 
+        const sftpServer = new transfer.CfnServer(this, 'SftpServer', {
+            endpointType: 'PUBLIC',
+            identityProviderType: 'SERVICE_MANAGED',
+            protocols: ['SFTP'],
+            loggingRole: sftpRole.roleArn,
+        });
 
-    // Create an SFTP user with EFS as the home directory
-    new transfer.CfnUser(this, 'SftpUser', {
-      serverId: sftpServer.ref,
-      userName: 'mySftpUser',
-      role: sftpRole.roleArn,
-      homeDirectory: `/${accessPoint.accessPointId}/spa`,  // Use the ID of the access point and the specified path
-    });
+        // Create an SFTP user with EFS as the home directory
+        const sftpUser = new transfer.CfnUser(this, 'SftpUser', {
+            serverId: sftpServer.attrServerId,
+            userName: 'mySftpUser',
+            role: sftpRole.roleArn,
+            homeDirectory: `/${accessPoint.accessPointId}/spa`,
+        });
 
-    // Outputs
-    new cdk.CfnOutput(this, 'SftpEndpoint', {
-      value: sftpServer.attrServerId + '.server.transfer.amazonaws.com',
-      description: 'SFTP Endpoint',
-    });
+        sftpUser.node.addDependency(sftpServer);
 
-    this.fileSystem = fileSystem;
-  }
+
+        // Outputs
+        new cdk.CfnOutput(this, 'SftpEndpoint', {
+            value: sftpServer.attrServerId + '.server.transfer.amazonaws.com',
+            description: 'SFTP Endpoint',
+        });
+
+        this.fileSystem = fileSystem;
+    }
 }
