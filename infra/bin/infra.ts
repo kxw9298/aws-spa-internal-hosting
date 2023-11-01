c700630767a6f0133abc19957667e5b439677c90
Keith Wang <kewang2011fall@gmail.com>
2023-11-01 18:20:45 +0000

    add self-signed cert

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -7,21 +7,23 @@ import { S3Stack } from "../lib/s3-stack";
 import { ECSStack } from '../lib/ecs-stack';
 import { ECRStack } from '../lib/ecr-stack';
 import { SftpEfsStack } from '../lib/sftp-efs-stack';
+import { MySslCertStack } from '../lib/ssl-stack';
+
 
 const app = new cdk.App();
 
 const s3Stack = new S3Stack(app, 'MyS3Stack');
 
-const vpcStack = new VpcStack(app, 'MyVpcStack');
-
 const ecrStack = new ECRStack(app, 'MyECRStack');
 
-// const sftpEfsStack = new SftpEfsStack(app, 'MySftpEfsStack', { vpc: vpcStack.vpc });
-// sftpEfsStack.addDependency(vpcStack);
 
-const ecsStack = new ECSStack(app, 'MyECSStack', {
-  vpc: vpcStack.vpc,
-  nginxRepoName: ecrStack.nginxRepo.repositoryName,
-}
-)
-ecsStack.addDependency(vpcStack);
+const sslStack = new MySslCertStack(app, 'MySslCertStack');
+
+// const vpcStack = new VpcStack(app, 'MyVpcStack');
+
+// const ecsStack = new ECSStack(app, 'MyECSStack', {
+//   vpc: vpcStack.vpc,
+//   nginxRepoName: ecrStack.nginxRepo.repositoryName,
+// }
+// )
+// ecsStack.addDependency(vpcStack);
