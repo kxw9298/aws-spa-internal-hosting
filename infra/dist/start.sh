63e44efcef22d0e3beb14a8d7c0b111c14377bcd
Keith Wang <kewang2011fall@gmail.com>
2023-11-01 19:00:36 +0000

    update docker image

--- infra/dist/start.sh
+++ infra/dist/start.sh
@@ -13,5 +13,11 @@ ls -al $APP_DIR
 # Copy assets from EFS to Nginx directory
 cp -r $APP_DIR/* /usr/share/nginx/html/
 
+# Save the SSL_PRIVATE_KEY environment variable to a file
+echo "$SSL_PRIVATE_KEY" > /etc/nginx/ssl/nginx.key
+
+# Save the SSL_CERTIFICATE environment variable to a file
+echo "$SSL_CERTIFICATE" > /etc/nginx/ssl/nginx.crt
+
 # Start Nginx
 nginx -g 'daemon off;'
