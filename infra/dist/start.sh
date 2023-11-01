fc88912b3506a3167019567356b63c0c665850cb
Keith Wang <kewang2011fall@gmail.com>
2023-11-01 20:28:13 +0000

    refactor

--- infra/dist/start.sh
+++ infra/dist/start.sh
@@ -13,11 +13,17 @@ ls -al $APP_DIR
 # Copy assets from EFS to Nginx directory
 cp -r $APP_DIR/* /usr/share/nginx/html/
 
+# Create the directory where SSL certificates will be stored
+mkdir -p /etc/nginx/ssl
+
 # Save the SSL_PRIVATE_KEY environment variable to a file
 echo "$SSL_PRIVATE_KEY" > /etc/nginx/ssl/nginx.key
 
 # Save the SSL_CERTIFICATE environment variable to a file
 echo "$SSL_CERTIFICATE" > /etc/nginx/ssl/nginx.crt
+
+# Adjust permissions of the SSL certificate and key
+chmod 600 /etc/nginx/ssl/nginx.*
 
 # Start Nginx
 nginx -g 'daemon off;'
